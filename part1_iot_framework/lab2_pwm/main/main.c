ca482cbeede0e45de4e9ea0dc657047f1e30f26d
REBAHMKHAIRICABRIT <mathis.cabrit@etu.unice.fr>
2023-11-30 14:54:20 +0100

    lab12

--- part1_iot_framework/lab2_pwm/main/main.c
+++ part1_iot_framework/lab2_pwm/main/main.c
@@ -8,85 +8,111 @@
 /**
  * @file main.c
  * @author Fabrice Muller
- * @date 02 Nov. 2021
- * @brief File containing the lab2 of Part 4.
+ * @date 31 Oct. 2021
+ * @brief File containing the lab1-2 of Part 4.
  *
  * @see https://github.com/fmuller-pns/esp32-vscode-project-template
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOSConfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/ledc.h"
-#include "esp_err.h"
 
-#include "soc/ledc_reg.h"
+#include "esp_timer.h"
+#include "esp_sleep.h"
 
+#include "driver/gpio.h"
+#include "driver/dac.h"
+
+#include "driver/adc.h"
+#include "driver/adc_common.h"
+#include "esp_adc_cal.h"
 
 /*
+ADC : Analog to Digital Converter
 Documentation:
-https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/ledc.html
+https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html
+
+
+Command to obtain Vref: 
+espefuse.py --port /dev/ttyUSB0 adc_info
 
 */
+
+/* Default eFuse Vref in mV */
+#define DEFAULT_VREF    1107
+
+const adc_unit_t unit = ADC_UNIT_1;
+const adc_channel_t channel = ADC1_CHANNEL_1;   
+const adc_bits_width_t width = ADC_WIDTH_BIT_10;
+const adc_atten_t atten = ADC_ATTEN_DB_2_5;
+
+
+esp_adc_cal_characteristics_t *adc_chars;
+
+/**
+ * @brief Print the Characterization type.
+ * 
+ * @param val_type type of characterization
+ */
+static void print_char_val_type(esp_adc_cal_value_t val_type) {
+	switch (val_type) {
+		case ESP_ADC_CAL_VAL_EFUSE_TP:
+        	printf("Characterized using Two Point Value\n");
+			break;
+		case ESP_ADC_CAL_VAL_EFUSE_VREF:
+        	printf("Characterized using eFuse Vref (%d mV)\n", DEFAULT_VREF);
+			break;
+		default:
+        	printf("Characterized using Default Vref\n");
+    }
+}
+
 
 /**
  * @brief Starting point function
  * 
  */
+
 void app_main(void) {
 
-  /* 
-   * LEDC Timer configuration
-   * Timer 0, Low speed mode, Auto clk, Résolution 10bits, Frequency 5 KHz
-   */
-  ledc_timer_config_t timer = {
-      .speed_mode = ??,
-      .duty_resolution = ??,
-      .timer_num = ??,
-      .freq_hz = ??,
-      .clk_cfg = ??};
+	/* Configure ADC */
+	adc1_config_width(width);	
+   	adc1_config_channel_atten(channel, atten);
 
-  ledc_timer_config(&timer);
+    /* Characterize ADC */
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
 
-  /*
-   * Channel configuration
-   * Timer 0, Channel 0, GPIO21 pin, Low speed mode, duty=0
-   */
-  ledc_channel_config_t channel = {
-      .gpio_num = ??,
-      .speed_mode = ??,
-      .channel = ??,  
-      .timer_sel = ??,
-      .duty = ??,    
-      .hpoint = 0
-      };
+	uint16_t measuredValues;
+	int v;
 
-  ledc_channel_config(&channel);
+	for (;;) {
 
-  /* Fade installation */
-  esp_err_t result = ledc_fade_func_install(0);
-  if (result != ESP_OK) {
-    printf("Error installing fade: %04x\n", result);  
-    return;
-  }
+		/* Get start time */
+		int64_t t1 = esp_timer_get_time();
 
-  /* First scenario */
-  printf("First scenario\n");
+		/* Perform measure */
+		v = adc1_get_raw(channel);
+		measuredValues = esp_adc_cal_raw_to_voltage(v, adc_chars);
 
-  for (int i = 0; i < 1024; i++) {
-    ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0,i,0);
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
+		/* Get end time */
+		int64_t t2 = esp_timer_get_time();
 
-  /* Second scenario */
-  printf("Second scenario\n");
+		/* Print time conversion and voltage */
+		printf("full time conversion = %lld µs\n", t2-t1);
+		printf("V = %d mV\n", measuredValues);
 
-  while(true) {
-    printf("Action 1\n");
-    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 0, 2000, LEDC_FADE_WAIT_DONE);
+		/* Every second */
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
 
-    printf("Action 2\n");
-    ledc_set_fade_time_and_start(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, 1024, 5000, LEDC_FADE_WAIT_DONE);
-  }
-
+	/* to ensure its exit is clean */
+	vTaskDelete(NULL);
 }
+
